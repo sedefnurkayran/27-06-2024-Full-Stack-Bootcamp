b25b731f8cdb74c01d587d619de3409797daa9f7
Sedefnur Kayran <109733884+sedefnurkayran@users.noreply.github.com>
2024-07-13 15:11:17 +0300

    .

--- Backend/Techcareer/Controllers/HomeController.cs
+++ Backend/Techcareer/Controllers/HomeController.cs
@@ -9,24 +9,27 @@ public class HomeController : Controller
 {
     public HomeController()
     {
-        
+
     }
 
     public IActionResult Index(string searchString, string category)
     {
         var products = Repository.Products;
 
-        if(!String.IsNullOrEmpty(searchString)){
+        if (!String.IsNullOrEmpty(searchString))
+        {
 
             ViewBag.SearchString = searchString;
-            products = products.Where(p=>p.Name!.ToLower().Contains(searchString)).ToList();
+            products = products.Where(p => p.Name!.ToLower().Contains(searchString)).ToList();
         }
-        if(!String.IsNullOrEmpty(category)&&category != "0"){
-            products = products.Where(p=>p.CategoryId == int.Parse(category)).ToList();
+        if (!String.IsNullOrEmpty(category) && category != "0")
+        {
+            products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();
         }
 
-     //   ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId","Name",category);
-        var model = new ProductViewModel{
+        //   ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId","Name",category);
+        var model = new ProductViewModel
+        {
             Products = products,
             Categories = Repository.Categories,
             SelectedCategory = category
@@ -38,46 +41,54 @@ public class HomeController : Controller
     [HttpGet]
     public IActionResult Create()
     {
-        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId","Name");
+        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
         return View();
     }
 
     [HttpPost]
     public async Task<IActionResult> Create(Product model, IFormFile imageFile)
     {
-        var allowenExtensions = new[] {".jpg",".png",".jpeg"};
-        if(imageFile != null){
+        var allowenExtensions = new[] { ".jpg", ".png", ".jpeg" };
+        if (imageFile != null)
+        {
             var extensions = Path.GetExtension(imageFile.FileName).ToLowerInvariant();
-            if(!allowenExtensions.Contains(extensions)){
-                ModelState.AddModelError("","Geçerli bir resim seçiniz!");
-            }else{
+            if (!allowenExtensions.Contains(extensions))
+            {
+                ModelState.AddModelError("", "Geçerli bir resim seçiniz!");
+            }
+            else
+            {
                 var randomFileName = string.Format($"{Guid.NewGuid().ToString()}{extensions}");
-                var path = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/img",randomFileName);
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img", randomFileName);
 
                 try
                 {
-                    using(var stream = new FileStream(path,FileMode.Create)){
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
                         await imageFile.CopyToAsync(stream);
                     }
                     model.Image = randomFileName;
                 }
                 catch
                 {
-                    ModelState.AddModelError("","Dosya yüklenirken bir hata oluştu.");
+                    ModelState.AddModelError("", "Dosya yüklenirken bir hata oluştu.");
                 }
             }
-        }else{
+        }
+        else
+        {
             ModelState.AddModelError("", "Bir dosya seçiniz.");
         }
 
-        if(ModelState.IsValid){
-        
-        model.ProductId = Repository.Products.Count+1;
-        Repository.CreateProduct(model);
-        return RedirectToAction("Index");
+        if (ModelState.IsValid)
+        {
+
+            model.ProductId = Repository.Products.Count + 1;
+            Repository.CreateProduct(model);
+            return RedirectToAction("Index");
         }
 
-        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId","Name");
+        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
         return View(model);
     }
 
